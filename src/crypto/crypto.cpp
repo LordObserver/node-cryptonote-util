5919b8892275ff63a7c4b389577d31f492771649
LordObserver <38561485+LordObserver@users.noreply.github.com>
2018-04-20 21:14:44 +0800

    Update crypto.cpp

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -209,14 +209,14 @@ namespace crypto {
     ge_scalarmult(&point2, &sec, &point);
     ge_tobytes(&image, &point2);
   }
-
+struct ec_point_pair {
+  ec_point a,b;
+}
 PUSH_WARNINGS
 DISABLE_VS_WARNINGS(4200)
   struct rs_comm {
     hash h;
-    struct {
-      ec_point a, b;
-    } ab[];
+    struct ec_point_pair ab[];
   };
 POP_WARNINGS
 
